51a4361871a2cd5fd3f807dc14a7206e3dd6a3f8
Alvin Ahl <125873667+AlvinAhl@users.noreply.github.com>
2024-02-06 14:01:19 +0100

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -1,2 +1,14 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+inköpslista = [43,12,54,18,43]
+inköpslista.extend([14])
+inköpslista.insert(4, 53)
+print(inköpslista)
+inköpslista.remove(18)
+print(inköpslista)
+inköpslista.sort
+medel = sum(inköpslista)/ len(inköpslista)
+print("medel:", medel)
+
+import random 
+resultat = [random.randint(1,6) for _ in range (33)]
+antal_treor = resultat.count(3)
+print(antal_treor)
